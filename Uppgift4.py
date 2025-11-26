25d9293ba25bba18b3a3c3e7272b5d033fc45155
Bullengit <isa.riedel@hotmail.com>
2025-11-26 15:56:24 +0100

    Färdig 26/11

---
+++ Uppgift4.py
@@ -0,0 +1,14 @@
+target_ip = "10.10.10.10"
+open_ports = [20, 30, 40]
+vulnerable = True
+
+print("Target IP is",target_ip)
+print(type(target_ip))
+print("\n")
+
+print("The open ports on the network is", open_ports)
+print(type(open_ports))
+print("\n")
+
+print ("Vulnerable network:", vulnerable)
+print(type(vulnerable))
