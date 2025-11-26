5b4b90d7e9cc417b1d407865c7386f292c84f15e
Bullengit <isa.riedel@hotmail.com>
2025-11-26 15:11:18 +0100

    test

--- Test1.py
+++ Test1.py
@@ -1,1 +1,17 @@
-print ("hello")
+text = ("hello")
+print (text [0])
+# text[0] = 'h'
+new_text = 'h' + text[1:]
+print (new_text)
+
+word = "Python"
+new_word = word[:2] + 'T' + word[3:]
+print (new_word)
+
+part1 = "Login"
+part2 = "success"
+message = part1 + '-' + part2
+print (message)
+
+
+#hejsan
